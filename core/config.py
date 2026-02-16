358b14eec6ac19f33a418589062e12e71221ea77
Adray-K <karabets11@gmail.com>
2026-02-16 12:32:23 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    db_url: str = "postgresql+asyncpg://postgres:1884@localhost:5432/Shop"
+    db_url: str = "postgresql+asyncpg://postgres:####@localhost:5432/Shop"
     db_echo: bool = True
 
 
@@ -13,5 +13,5 @@ settings = Settings()
 # DB_HOST = "localhost"
 # DB_PORT = 5432
 # DB_USER = "postgres"
-# DB_PASS = 1884
+# DB_PASS = ####
 # DB_NAME = "Books"
